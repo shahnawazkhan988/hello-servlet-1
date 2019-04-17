832e6e783d42d87c69cd5e56ee3c54e3b2f3a869
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-04-17 13:40:07 +0200

    doPost

--- hello-servlet/hello-servlet/src/main/java/com/examples/helloservlet/HelloWorldServlet.java
+++ hello-servlet/hello-servlet/src/main/java/com/examples/helloservlet/HelloWorldServlet.java
@@ -1,10 +1,13 @@
 package com.examples.helloservlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * Servlet implementation class HelloWorldServlet
@@ -17,19 +20,28 @@ public class HelloWorldServlet extends HttpServlet {
 	 */
 	public HelloWorldServlet() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter()
 			.append("Served at: ")
 			.append(request.getContextPath());
 	}
 
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String name = request.getParameter("name");
+		if (StringUtils.isEmpty(name))
+			name = "World";
+		request.setAttribute("user", name);
+		request.getRequestDispatcher("response.jsp")
+			.forward(request, response);
+	}
 }
